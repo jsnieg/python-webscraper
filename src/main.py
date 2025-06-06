8d26573482d9fc31f9b447bcfdba2de23bb30e56
jsnieg <jasjasiaczek02@gmail.com>
2025-06-06 14:47:10 +0100

    web scraping library -> playwright

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
-def main() -> None:
+# Requirements
+# - python 3.12
+# - playwright
+
+# Libraries
+import asyncio
+#import playwirght.async_api import async_playwright
+
+async def main() -> None:
     print("It's empty here, but hopefully we'll get some basic web scraping going.")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())

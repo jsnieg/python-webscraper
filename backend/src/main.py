41c7e01c9937c02e10cff3de202fb35c901ce23e
jsnieg <jasjasiaczek02@gmail.com>
2025-06-10 14:37:37 +0100

    working scrape of movies, need to scrape img which seems to be on letterboxd db

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,6 @@
 # Requirements
 # - python 3.12
-# - beautifulsoup
+# - beautifulsoup4
 # - colorama
 # - django
 
@@ -9,6 +9,7 @@ import asyncio
 import django
 import urllib.parse
 import requests
+import re
 from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright
 from colorama import init as colorama_init
@@ -19,25 +20,34 @@ unavailable_version: str = "VERSION UNAVAILABLE"
 #async def main() -> None:
 #    pass
 
+# TODO;
+# - scrape first 'n' number of movies visible
+
 def main() -> None:
     print(f'Initialising colorama to give console text color')
     colorama_init()
     print('Initialised.')
 
     data: list = []
-    URL: str = 'https://letterboxd.com/films/popular/upcoming/'
+    # URL: str = 'https://letterboxd.com/films/popular/upcoming/'
+    URL: str = 'https://letterboxd.com/films/ajax/popular/upcoming/?esiAllowFilters=true'
     print(f"Sending a get request to URL {Fore.BLUE}[{URL}]{Style.RESET_ALL}")
     soup = BeautifulSoup(
         requests.get(url=URL).text,
-        features="html.parser" # gets rid of warning
+        features="html.parser", # gets rid of warning
     )
-    print(f'What a Beautiful Soup!')
+    # print(f'What a Beautiful Soup!')
     # print(soup)
     for lbx in soup.select('li.listitem'):
         data.append({
             'title': lbx.img.get('alt'),
+            'image_src': lbx.img.get('src'),
+            'image_srcset': lbx.img.get('srcset'),
         })
     print(data)
+    
+    # found = soup.find_all("li", class_="listitem poster-container")
+    # print(found)
 
 if __name__ == "__main__":
     main()

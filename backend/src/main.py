8da6d37babf2084bf9ead863b88beda4ce1a556b
jsnieg <jasjasiaczek02@gmail.com>
2025-06-11 10:56:04 +0100

    improving code readability

--- backend/src/main.py
+++ backend/src/main.py
@@ -17,7 +17,6 @@ from colorama import init as colorama_init
 from colorama import Fore, Style
 
 unavailable_version: str = "VERSION UNAVAILABLE"
-_datetime: datetime = datetime.datetime.now()
 
 #async def main() -> None:
 #    pass
@@ -28,6 +27,9 @@ _datetime: datetime = datetime.datetime.now()
 
 def scrape_page(urls: list[str]=None, URL: str="") -> BeautifulSoup:
     """Method for scraping a list of URL pages/URL."""
+    _datetime: datetime = datetime.datetime.now()
+    print(f"[{_datetime}] Sending a get request to URL {Fore.BLUE}[{URL}]{Style.RESET_ALL}")
+    print(f'[{_datetime}] {Fore.GREEN}Scraping movie general information...{Style.RESET_ALL}')
     soup = BeautifulSoup(
         markup=requests.get(url=URL).text,
         features="html.parser", # gets rid of warning
@@ -36,18 +38,11 @@ def scrape_page(urls: list[str]=None, URL: str="") -> BeautifulSoup:
 
 def scrape_movie_information(soup: BeautifulSoup) -> list[dict]:
     """Method for scraping basic information about the movies e.g., titles and img URL"""
-    pass
-
-def main() -> None:
-    colorama_init()
-
-    data: list = []
-    URL: str = 'https://letterboxd.com/films/ajax/popular/upcoming/?esiAllowFilters=true'
-    print(f"[{_datetime}] Sending a get request to URL {Fore.BLUE}[{URL}]{Style.RESET_ALL}")
-    print(f'[{_datetime}] {Fore.GREEN}Scraping movie general information...{Style.RESET_ALL}')
-    soup = scrape_page(URL=URL)
-
+    data: list[dict] = []
+    _datetime: datetime = datetime.datetime.now()
     number_of_movies: int = None
+
+    print(f"[{_datetime}] {Fore.GREEN}Scraping...{Style.RESET_ALL}")
     for lbx in soup.select('p.ui-block-heading'):
         # Remove all whitespaces in the <p></p>
         text: str = lbx.get_text().strip()
@@ -59,7 +54,6 @@ def main() -> None:
         number_of_movies: int = int(''.join(text))
     print(f'There are [{number_of_movies}] movies in total.')
 
-
     for lbx in soup.select('li.listitem'):
         data.append({
             'title': lbx.img.get('alt'),
@@ -67,7 +61,14 @@ def main() -> None:
             'image_srcset': lbx.img.get('srcset'),
         })
 
-    #print(data)
+    return data
+
+def main() -> None:
+    colorama_init()
+    URL: str = 'https://letterboxd.com/films/ajax/popular/upcoming/?esiAllowFilters=true'
+    soup: BeautifulSoup = scrape_page(URL=URL)
+    data: list[dict] = scrape_movie_information(soup)
+    print(data)
 
 if __name__ == "__main__":
     main()

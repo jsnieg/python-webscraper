6056f59ece2ac5260aba2e14b19dabdf187fc289
jsnieg <jasjasiaczek02@gmail.com>
2025-06-11 12:46:14 +0100

    first scraping for url in films container then using that to scrape movies

--- backend/src/main.py
+++ backend/src/main.py
@@ -11,7 +11,7 @@ import urllib.parse
 import requests
 import re
 import datetime
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from playwright.async_api import async_playwright
 from colorama import init as colorama_init
 from colorama import Fore, Style
@@ -27,13 +27,17 @@ unavailable_version: str = "VERSION UNAVAILABLE"
 
 def scrape_page(urls: list[str]=None, URL: str="") -> BeautifulSoup:
     """Method for scraping a list of URL pages/URL."""
+    soup = None
     _datetime: datetime = datetime.datetime.now()
-    print(f"[{_datetime}] Sending a get request to URL {Fore.BLUE}[{URL}]{Style.RESET_ALL}")
-    print(f'[{_datetime}] {Fore.GREEN}Scraping movie general information...{Style.RESET_ALL}')
-    soup = BeautifulSoup(
-        markup=requests.get(url=URL).text,
-        features="html.parser", # gets rid of warning
-    )
+    print(f"[{_datetime}] Sending a get request to URL\n{Fore.BLUE}[{URL}]{Style.RESET_ALL}")
+    print(f'[{_datetime}] {Fore.GREEN}Scraping the webpage...{Style.RESET_ALL}')
+    if urls is not None:
+        return
+    else:
+        soup = BeautifulSoup(
+            markup=requests.get(url=URL).text,
+            features="html.parser", # gets rid of warning
+        )
     return soup
 
 def scrape_movie_information(soup: BeautifulSoup) -> list[dict]:
@@ -42,7 +46,7 @@ def scrape_movie_information(soup: BeautifulSoup) -> list[dict]:
     _datetime: datetime = datetime.datetime.now()
     number_of_movies: int = None
 
-    print(f"[{_datetime}] {Fore.GREEN}Scraping...{Style.RESET_ALL}")
+    print(f"[{_datetime}] {Fore.GREEN}Scraping contents...{Style.RESET_ALL}")
     for lbx in soup.select('p.ui-block-heading'):
         # Remove all whitespaces in the <p></p>
         text: str = lbx.get_text().strip()
@@ -68,13 +72,28 @@ def scrape_movie_information(soup: BeautifulSoup) -> list[dict]:
     print(f'\t{Fore.RED}*{Style.RESET_ALL}There are potentially ~[{number_of_movies // individual_movies_count}] movies in this category.')
     return data
 
+def get_url_of_container(soup: BeautifulSoup) -> str | list[str]:
+    """Method for finding a url of the container to scrape."""
+    _datetime: datetime = datetime.datetime.now()
+    print(f"[{_datetime}] Scraping to get [data-url] of the film's container")
+    container: Tag = soup.find('div', {'id': 'films-browser-list-container'})
+    url_found: str | list[str] = container['data-url']
+    if url_found:
+        print(f"[{_datetime}] URL found!\n{Fore.BLUE}[{url_found}]{Style.RESET_ALL}")
+        return url_found
+    else:
+        print(f"[{_datetime}]{Fore.RED}URL was not found!{Style.RESET_ALL}")
+
 def main() -> None:
     colorama_init()
-    URL: str = 'https://letterboxd.com/films/ajax/popular/upcoming/?esiAllowFilters=true'
-    soup: BeautifulSoup = scrape_page(URL=URL)
-    data: list[dict] = scrape_movie_information(soup)
-    #print(data)
+    URL: str = "https://letterboxd.com"
+    PATH: str = "/films/popular/decade/2020s/"
+    web_soup: BeautifulSoup = scrape_page(URL=URL+PATH)
+    container_url: BeautifulSoup = get_url_of_container(web_soup)
+    container_soup: BeautifulSoup = scrape_page(URL=URL+container_url)
+    scrape_movie_information(container_soup)
 
+##########################    
 if __name__ == "__main__":
     main()
     # asyncio.run(main())

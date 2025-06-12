4967d689854dd64debe97b1d7f5838addb66dfd3
jsnieg <jasjasiaczek02@gmail.com>
2025-06-12 11:51:19 +0100

    working on scraping individual movie description and img psoter

--- backend/src/main.py
+++ backend/src/main.py
@@ -24,22 +24,23 @@ unavailable_version: str = "VERSION UNAVAILABLE"
 # - scrape first 'n' number of movies visible
 # - classes (oop)
 
-def timestamp_of_now():
-    """Method for calling timestamp of now."""
-    return datetime.datetime.now()
-
-def scrape_page(urls: list[str]=None, URL: str="") -> BeautifulSoup:
+def scrape_page(urls: list[str]=None, URL: str="") -> BeautifulSoup | None:
     """Method for scraping a list of URL pages/URL."""
     soup = None
-    print(f"[{timestamp_of_now()}] Sending a get request to URL\n{Fore.BLUE}[{URL}]{Style.RESET_ALL}")
-    print(f'[{timestamp_of_now()}] {Fore.GREEN}Scraping the webpage...{Style.RESET_ALL}')
-    if urls is not None:
-        return
+    print(f"[{datetime.datetime.now()}] Sending a get request to URL\n{Fore.BLUE}[{URL}]{Style.RESET_ALL}")
+    print(f'[{datetime.datetime.now()}] {Fore.GREEN}Scraping the webpage...{Style.RESET_ALL}')
+    if (urls is not None) or (URL is not None) or (URL != ""):
+        try:
+            soup = BeautifulSoup(
+                markup=requests.get(url=URL).text,
+                features="html.parser", # gets rid of warning
+            )
+        except Exception as _exception:
+            print(_exception)
+            return
     else:
-        soup = BeautifulSoup(
-            markup=requests.get(url=URL).text,
-            features="html.parser", # gets rid of warning
-        )
+        print(f'\t{Fore.RED}*{Style.RESET_ALL}No specified URL.')
+        return
     return soup
 
 def scrape_movie_information(soup: BeautifulSoup) -> list[dict]:
@@ -47,50 +48,62 @@ def scrape_movie_information(soup: BeautifulSoup) -> list[dict]:
     data: list[dict] = []
     number_of_movies: int = None
 
-    print(f"[{timestamp_of_now()}] {Fore.GREEN}Scraping contents...{Style.RESET_ALL}")
-    for lbx in soup.select('p.ui-block-heading'):
-        # Remove all whitespaces in the <p></p>
-        text: str = lbx.get_text().strip()
+    try:
+        print(f"[{datetime.datetime.now()}] {Fore.GREEN}Scraping contents...{Style.RESET_ALL}")
+        for lbx in soup.select('p.ui-block-heading'):
+            # Remove all whitespaces in the <p></p>
+            text: str = lbx.get_text().strip()
 
-        # Find all numbers within the string using regex
-        text: list = re.findall(r'\b\d+\b', text)
+            # Find all numbers within the string using regex
+            text: list = re.findall(r'\b\d+\b', text)
+            if text:
+                # Join string together and cast to int
+                number_of_movies: int = int(''.join(text))
+            else:
+                print(f'\t{Fore.RED}*{Style.RESET_ALL}No movies on this webpage.')
+        print(f'\t{Fore.RED}*{Style.RESET_ALL}There are [{number_of_movies}] movies in total.')
 
-        # Join string together and cast to int
-        number_of_movies: int = int(''.join(text))
-    print(f'\t{Fore.RED}*{Style.RESET_ALL}There are [{number_of_movies}] movies in total.')
+        individual_movies_count: int = 0
+        # Return all movie information
+        for lbx in soup.select('li.listitem'):
+            individual_movies_count += 1
+            data.append({
+                'title': lbx.img.get('alt'),
+                'url': lbx.find('div')['data-film-slug'],
+                'rating': float(lbx['data-average-rating']),
+            })
+        
+        average_rating: float = sum([d["rating"] for d in data]) / individual_movies_count
 
-    individual_movies_count: int = 0
-    # Return all movie information
-    for lbx in soup.select('li.listitem'):
-        individual_movies_count += 1
-        data.append({
-            'title': lbx.img.get('alt'),
-            'image_src': lbx.img.get('src'),
-            'image_srcset': lbx.img.get('srcset'),
-            'rating': float(lbx['data-average-rating']),
-        })
-    
-    average_rating: float = sum([d["rating"] for d in data]) / individual_movies_count
-
-    print(f'\t{Fore.RED}*{Style.RESET_ALL}There are [{individual_movies_count}] movies on this page.')
-    print(f'\t{Fore.RED}*{Style.RESET_ALL}There are potentially ~[{number_of_movies // individual_movies_count}] movies in this category.')
-    print(f'\t{Fore.RED}*{Style.RESET_ALL}Average rating is [{average_rating:.2f}] in this category.')
-    return data
+        print(f'\t{Fore.RED}*{Style.RESET_ALL}There are [{individual_movies_count}] movies on this page.')
+        print(f'\t{Fore.RED}*{Style.RESET_ALL}There are potentially ~[{number_of_movies // individual_movies_count}] movies in this category.')
+        print(f'\t{Fore.RED}*{Style.RESET_ALL}Average rating is [{average_rating:.2f}] in this category.')
+        print(f'\t{Fore.RED}*{Style.RESET_ALL}Example URL of the movie: [https://letterboxd.com/film/{[d['url'] for d in data][0]}]')
+        return data
+    except Exception as _exception:
+        print(_exception)
+        return
 
 def get_url_of_container(soup: BeautifulSoup) -> str | list[str]:
     """Method for finding a url of the container to scrape."""
     try:
-        print(f"[{timestamp_of_now()}] Scraping to get [data-url] of the film's container")
-        container: Tag = soup.find('div', {'id': 'films-browser-list-container'})
+        print(f"[{datetime.datetime.now()}] Scraping to get [data-url] of the film's container")
+        container: Tag = soup.find(name='div', attrs={'id': 'films-browser-list-container'})
         url_found: str | list[str] = container['data-url']
         if url_found:
-            print(f"[{timestamp_of_now()}] URL found!\n{Fore.BLUE}[{url_found}]{Style.RESET_ALL}")
+            print(f"[{datetime.datetime.now()}] URL found!\n{Fore.BLUE}[{url_found}]{Style.RESET_ALL}")
             return url_found
         else:
-            print(f"[{timestamp_of_now()}]{Fore.RED}URL was not found!{Style.RESET_ALL}")
+            print(f"[{datetime.datetime.now()}]{Fore.RED}URL was not found!{Style.RESET_ALL}")
             return
     except Exception as _exception:
+        print(f"[{datetime.datetime.now()}]{Fore.RED}URL was not found!{Style.RESET_ALL}")
         print(_exception)
+        return
+    
+def scrape_movie_details(url: str) -> list[dict]:
+    """Scraping individual movie details e.g., description or movie poster."""
+    pass
 
 def main() -> None:
     colorama_init()

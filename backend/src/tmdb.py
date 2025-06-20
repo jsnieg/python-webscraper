1d64d0fc4fe9a64efb24373a8e00ab29753c4bd1
jsnieg <jasjasiaczek02@gmail.com>
2025-06-20 14:01:48 +0100

    working get/post requests

--- backend/src/tmdb.py
+++ backend/src/tmdb.py
@@ -5,15 +5,60 @@ from bs4 import BeautifulSoup, ResultSet, Tag
 from colorama import init as colorama_init
 from colorama import Fore, Style
 
+from typing import Annotated
+from fastapi import Body, FastAPI, Request
+from pydantic import BaseModel, Field
+
 web_url = 'https://www.themoviedb.org'
 
+app = FastAPI()
+
 # TODO:
-# PUT request for creating URL to scrape
+# POST request for creating URL to scrape
 # default on React-end: [/movie/, /movie/top-rated/, /movie/upcoming/, /movie/now-playing/]
 # GET request after PUT request is called
 # Authentication Demo with OAuth 2.0 [?]
 # Support for scraping TV shows [?]
 # Scrape more than just one page [https://www.themoviedb.org/movie/now-playing?page=2] ?page=2 is the key, could influence increment page number by 1 to 3/5. Probably 3 for testing.
+
+
+
+class ScrapeURL(BaseModel):
+    """
+    A class for defining the URL as string on PUT reqeuest call.
+    """
+    # url: str | None = Field(
+    #     default=None,
+    #     title="A placeholder for the URL.",
+    #     max_length=300
+    # )
+    url: str | None
+
+@app.get("/test")
+async def test_api() -> dict:
+    """
+    Call this GET method to see if API is infact working as intended.
+    """
+    return {'Fast':'API'}
+
+@app.get("/get_url")
+async def get_url() -> dict:
+    """
+    A function that returns the set url when POST request was called.
+    """
+    return {'Response': app.state.url}
+
+@app.post("/set_url")
+# async def set_url(url: str | None, scrape_url: Annotated[ScrapeURL, Body(embed=True)]) -> str:
+async def set_url(url: str, scrape_url: ScrapeURL) -> dict:
+    """
+    A function that sets current str for usage.
+
+    Only acceptable URL is TMDB (https://www.themoviedb.org).
+    """
+    # https://stackoverflow.com/questions/71260288/how-to-share-variables-between-http-requests-in-fastapi
+    app.state.url = url
+    return {'Response': app.state.url}
 
 async def fetch(session: aiohttp.ClientSession, url: str = 'https://www.themoviedb.org/movie/top-rated/') -> str:
     """
